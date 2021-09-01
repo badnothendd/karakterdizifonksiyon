8fb7ddee78d6c4553c9b68e1ca079073c1ee4bca
badnothendd <78541390+badnothendd@users.noreply.github.com>
2021-09-01 16:32:50 +0300

    karakterdizifonksiyonornegi

---
+++ karktrdizifonk.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+ int uzunlukdon(char name[]) {
+ 	
+	 int uzunluk=0;
+ 	int i;
+ 	
+ 	 for (i=0; name[i] != '\0'; i++) {
+ 	 	
+ 	 	uzunluk++;
+	  }
+	  return uzunluk;
+ }
+
+
+
+
+
+
+
+
+
+int main() {
+	
+	
+	char isim[]= "tugba";
+	
+	
+	printf("%d", uzunlukdon(isim));
+
+	
+	return 0;
+}
